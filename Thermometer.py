026aba653bbd53fafefa720579fd1979cb5c1edd
daneengman <drengman7@gmail.com>
2022-11-29 17:32:47 -0500

    Update Thermometer.py
    Updates bus to 1

--- Thermometer.py
+++ Thermometer.py
@@ -9,15 +9,15 @@ class Thermometer:
         self.TEMP_ADDR = 0x48
 
         # is this the right bus? is it set up somewhere else?
-        self.bus = smbus.SMBus(0)
+        self.bus = smbus.SMBus(1)
 
-        #have something to check if device is connected? maybe later
+        #have something to check if device is connected?
 
     def get_measurement(self):
-        t_low = self.bus.read_byte_data(self.TEMP_ADDR, 0b10)
-        t_high = self.bus.read_byte_data(self.TEMP_ADDR, 0b11)
+        tLow = self.bus.read_byte_data(self.TEMP_ADDR, 0b10)
+        tHigh = self.bus.read_byte_data(self.TEMP_ADDR, 0b11)
 
-        bit_value = (t_high<<4) | (t_low>>4);
+        bit_value = (tHigh<<4) | (tLow>>4);
         # do negatives need to be handled? in that case, fix this
         # if((bit_value & 0x800) != 0):
         #     # this means we have a negative number
